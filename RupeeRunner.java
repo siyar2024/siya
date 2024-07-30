87b95e38ee83c96e77da137052e855e64c327904
unknown <siyar2024>
2024-07-30 22:05:25 +0530

    new files

---
+++ RupeeRunner.java
@@ -0,0 +1,8 @@
+class DrillRunner{
+	public static void main(String[] args)
+	{
+		System.out.println("start main in DrillRunner");
+		System.out.print("end main in DrillRunner");
+		System.err.println("end main in DrillRunner");
+	}
+}
